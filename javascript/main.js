e093e46fd531ac5abf406f335a59a639b5acd740
Adalida-Lewis <42812369+Adalida-Lewis@users.noreply.github.com>
2020-03-02 10:49:37 -0600

    Got rid of extra values

--- javascript/main.js
+++ javascript/main.js
@@ -1,22 +1,16 @@
 
 $(document).ready(function(){
-    
+
     function checker(sentence){
         let wordsleft = 0;
         let maxLength = 250 - 50;
         wordsleft = maxLength - sentence.length;
         return 250 - sentence.length;
     }
-    
-document.getElementById("enteredSentence").addEventListener("click", function(){
-    let enteredSentences = document.getElementById("sentence").value;
-    
-    let count = 0;
-    for (i = 0; i < enteredSentences.length; i++) {
-  console.count();
-}
 
-    $("#wordsLeft").html(checker(enteredSentences));
-})
-    
+    document.getElementById("enteredSentence").addEventListener("click", function(){
+        let enteredSentences = document.getElementById("sentence").value;
+        $("#wordsLeft").html(checker(enteredSentences));
     })
+
+})
