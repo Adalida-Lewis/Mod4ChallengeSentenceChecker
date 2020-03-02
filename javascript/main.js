6e96a124cf1ed4f9b6e2f27fd495cb6326eaeef2
Adalida-Lewis <42812369+Adalida-Lewis@users.noreply.github.com>
2020-03-02 10:48:10 -0600

    Fixed Values
    Correcting values

--- javascript/main.js
+++ javascript/main.js
@@ -3,17 +3,19 @@ $(document).ready(function(){
     
     function checker(sentence){
         let wordsleft = 0;
-        let maxLength = 250 + 50;
+        let maxLength = 250 - 50;
         wordsleft = maxLength - sentence.length;
-        return sentence.length;
+        return 250 - sentence.length;
     }
     
 document.getElementById("enteredSentence").addEventListener("click", function(){
-    let enteredSentences = document.getElementById("sentence").innerHTML;
+    let enteredSentences = document.getElementById("sentence").value;
+    
     let count = 0;
     for (i = 0; i < enteredSentences.length; i++) {
   console.count();
 }
+
     $("#wordsLeft").html(checker(enteredSentences));
 })
     
